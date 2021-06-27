e65a49cb770525fa381fc68ca1198c44490bd58b
franponce <franponce98@gmail.com>
2021-06-27 19:34:00 -0300

    Funciones flecha

--- NodeJS/primera-app/app.js
+++ NodeJS/primera-app/app.js
@@ -20,3 +20,28 @@ console.log(nombre6);
 // nombre6 = "Ana"; // Assigment to constant variable. 
 
 
+// Funciones flecha
+// Funciones en ES5
+
+// Constante de años que es un arreglo y queremos que nos devuelva la diferencia en añis entre el año actual y el los del array. 
+
+const years = [2000, 2005, 2008, 20212];
+
+var edad5 = years.map(function(el){
+    return 2021 - el;
+});
+
+console.log(edad5);
+
+// Funciones en ES6 de tipo flecha
+let edad6= years.map((el) => {
+    return 202021 - el;
+});
+
+console.log(edad6);
+
+// Mismo resutlado y podemos reducir el código más. Cómo solo nos devuelve una sola línea, lo podemos dejar así:
+
+let edad7= years.map(el => 2021 - el);
+console.log(edad7);
+
