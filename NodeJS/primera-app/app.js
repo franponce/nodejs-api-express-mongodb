157f2f49df9cd03ad5a7fdfb64fbd5f5dcdb3e3e
franponce <franponce98@gmail.com>
2021-06-27 19:19:15 -0300

    Variables con const, let y var

--- NodeJS/primera-app/app.js
+++ NodeJS/primera-app/app.js
@@ -4,3 +4,19 @@ function saludo(nombre){
 
 saludo("Franco");
 
+// ES5 - Variables
+
+var nombre = "Franco";
+console.log(nombre);
+
+nombre = "Pablo";
+console.log(nombre);
+
+// ES6+ - Variables
+
+const nombre6 = "Luis";
+let edad = 35;
+console.log(nombre6);
+// nombre6 = "Ana"; // Assigment to constant variable. 
+
+
